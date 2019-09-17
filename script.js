562313c07e3cb4b2d6b44bd08ce9f0aca1176ab0
Pradeep Bhattarai <29733866+pr0d33p@users.noreply.github.com>
2019-09-17 21:14:40 +0545

    updated

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+// function generate() {
+// 	var password = '';
+
+// 	var length = all.length;
+// 	alert(all)
+// }
+
 function generate() {
+	var result = ''
 	capital = document.getElementsByName('capital')[0].value
 	small = document.getElementsByName('small')[0].value
 	number = document.getElementsByName('number')[0].value
 	chars = document.getElementsByName('chars')[0].value
-	all = capital + small + number + chars
-	alert(all)
+	characters = capital + small + number + chars
+	var charactersLength = characters.length;
+	length = document.getElementsByName('length')[0].value
+
+	for ( var i = 0; i < length; i++ ) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	
+	document.getElementById('generatedPassword').setAttribute('style', "display:block;")
+	document.getElementById('password').setAttribute('value', result);
 }
 
 function date() {
 	var d = new Date();
 	document.write(d.getFullYear())
 }
+
